0ae8ae0f7fdc18ac5863ad025f3d9a18eda49355
esantos2002@hotmail.com <emersonary>
2025-09-19 21:50:58 -0300

    deploy test5

--- apps/node-app/src/index.ts
+++ apps/node-app/src/index.ts
@@ -66,7 +66,7 @@ async function deribitVerificationCycle() {
       now.toISOString(),
       "delta:",
       btcSummary.delta_total,
-      "price5:",
+      "price6:",
       ticker.last_price);
 
 
